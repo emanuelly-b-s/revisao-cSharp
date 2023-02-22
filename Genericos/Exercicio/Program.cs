08ffeec4d27313c5a95c4a2d1647acdb50f6f3ea
-Manu <emanuelly.bs14@gmail.com>
2023-02-22 15:05:59 -0300

    genericos

---
+++ Genericos/Exercicio/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+Stack<int> teste = new Stack<int>();
+
+
+teste.Push(1);
+teste.Push(2);
+teste.Pop();
+
+
+Console.WriteLine(teste);
+
+public class Stack<T>
+{
+
+    private T[] pilha = new T[10];
+    private int topo = -1;
+
+    public void Push(T item)
+    {
+        topo++;
+        pilha[topo] = item;
+    }
+
+    public void Pop()
+    {
+        if (topo < 0)
+            return;
+        pilha[topo] = default(T);
+        topo--;
+    }
+
+    public bool isEmpty()
+    {
+        return topo < 0;
+    }
+
+    public int Size()
+    {
+        return topo + 1;
+    }
+
+    void Clear()
+    {
+        for (int i = 0; i < pilha.Count(); i++)
+        {
+            pilha[i] = default(T);
+        }
+
+        topo = -1;
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = topo; i < 0; i--)
+        {
+            result +=$"{i}" + "[{pilha[i]}]";
+        }
+        return result;
+    }
+
+}
